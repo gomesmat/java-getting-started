2f471346ef649e59902f18c00934323a3ad52a62
matheusribeirogomes <matheusribeirogomes@hotmail.com>
2018-12-05 19:08:39 -0500

    a

--- src/test/java/TesteFato.java
+++ src/test/java/TesteFato.java
@@ -1,32 +1,32 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-import com.example.Fibonacci;
+import com.example.Fatorial;
 
-public class TesteFibo {
+public class TesteFato {
 
     @Test
     public void Teste1() {
-        Fibonacci f = new Fibonacci();
-        assertEquals(1L, f.fibo(1));
+        Fatorial f = new Fatorial();
+        assertEquals(1L, f.fatorial(1));
     }
     @Test
     public void Teste2() {
-        Fibonacci f = new Fibonacci();
-        assertEquals(2L, f.fibo(3));
+        Fatorial f = new Fatorial();
+        assertEquals(2L, f.fatorial(3));
     }
     @Test
     public void Teste3() {
-        Fibonacci f = new Fibonacci();
-        assertEquals(34L, f.fibo(9));
+        Fatorial f = new Fatorial();
+        assertEquals(34L, f.fatorial(9));
     }
     @Test
     public void Teste4() {
-        Fibonacci f = new Fibonacci();
-        assertEquals(233L, f.fibo(13));
+        Fatorial f = new Fatorial();
+        assertEquals(233L, f.fatorial(13));
     }
     @Test
     public void Teste5() {
-        Fibonacci f = new Fibonacci();
-        assertEquals(514229L, f.fibo(29));
+        Fatorial f = new Fatorial();
+        assertEquals(514229L, f.fatorial(29));
     }
 }
